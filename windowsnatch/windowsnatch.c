a2ce502c4151a74e26396b6e626448f5e157483b
Aviv Eyal <avivey@gmail.com>
2015-05-30 16:34:51 -0700

    repeate magic

--- windowsnatch/windowsnatch.c
+++ windowsnatch/windowsnatch.c
@@ -14,6 +14,7 @@ TCHAR *targetTitle = TEXT("target");
 
 HWND targetWindow = NULL;
 
+void showMagic();
 void findMyPutty();
 void HandleWinEvent(HWINEVENTHOOK hook, DWORD event, HWND hwnd,
                     LONG idObject, LONG idChild,
@@ -22,22 +23,65 @@ void HandleWinEvent(HWINEVENTHOOK hook, DWORD event, HWND hwnd,
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prev, LPSTR cmdline, int show)
 {
-  int len;
   HWND hWnd;
   MSG msg;
   BOOL bRet;
   HWINEVENTHOOK g_hook;
+  DWORD hProc = 0; //putty proc
 
   findMyPutty();
   if (targetWindow == NULL) {
     printf("didn't find the target window");
     return 1;
   }
+  showMagic();
 
-  len = GetWindowText(targetWindow, buff, LEN_BUFF_LONG);
-  printf("Title len = %d\n[%S]\n", len, buff);
+  GetWindowThreadProcessId(targetWindow, &hProc);
+  if (hProc == 0) {
+    printf("no process to call\n");
+    return 3;
+  }
 
-  TCHAR magicMarker = buff[lstrlen(targetTitle)];
+  RegisterApplicationClass(hInst);
+
+  hWnd = CreateWindow(
+           THIS_CLASSNAME, THIS_TITLE,
+           0, // style.
+           0, 0, 0, 0, // location, size
+           HWND_MESSAGE, // Message-only window.
+           NULL, hInst, NULL);
+
+  g_hook =  SetWinEventHook(
+              EVENT_OBJECT_NAMECHANGE, EVENT_OBJECT_NAMECHANGE,
+              NULL, // Handle to DLL.
+              HandleWinEvent, // The callback.
+              hProc, 0, // Process and thread IDs of interest (0 = all)
+              WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS);
+  if (g_hook == 0) {
+    printf("Failed to SetWinEventHook\n");
+    return 2;
+  }
+
+  //  Message loop
+  while (TRUE) {
+    bRet = GetMessage(&msg, NULL, 0, 0);
+    if (bRet == 0 || bRet == -1)
+      break;
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+
+  UnregisterClass(THIS_CLASSNAME, hInst);
+
+  return 0;
+}
+
+void showMagic()
+{
+  int len = GetWindowText(targetWindow, buff, LEN_BUFF_LONG);
+  // printf("Title len = %d\n[%S]\n", len, buff);
+
+  TCHAR magicMarker = len > lstrlen(targetTitle) ? buff[lstrlen(targetTitle)] : 0;
   switch (magicMarker) {
   case 0:
     printf("NULL char");
@@ -56,44 +100,15 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prev, LPSTR cmdline, int show)
     break;
   }
   printf("\n");
-
-  RegisterApplicationClass(hInst);
-
-  hWnd = CreateWindow(
-           THIS_CLASSNAME, THIS_TITLE,
-           0, // style.
-           0, 0, 0, 0, // location, size
-           HWND_MESSAGE, // Message-only window.
-           NULL, hInst, NULL);
-
-  g_hook =  SetWinEventHook(
-              EVENT_OBJECT_NAMECHANGE, EVENT_OBJECT_NAMECHANGE,
-              NULL, // Handle to DLL.
-              HandleWinEvent, // The callback.
-              0, 0,              // Process and thread IDs of interest (0 = all)
-              WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS);
-
-// CreateWindow with parent = HWND_MESSAGE  for message-only (?).
-
-  //  Message loop
-  while (TRUE) {
-    bRet = GetMessage(&msg, NULL, 0, 0);
-    if (bRet == 0 || bRet == -1)
-      break;
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-  }
-
-  UnregisterClass(THIS_CLASSNAME, hInst);
-
-  return 0;
 }
 
 void HandleWinEvent(HWINEVENTHOOK hook, DWORD event, HWND hwnd,
                     LONG idObject, LONG idChild,
                     DWORD dwEventThread, DWORD dwmsEventTime)
 {
-
+  if (event == EVENT_OBJECT_NAMECHANGE) {
+    showMagic();
+  }
 }
 
 
