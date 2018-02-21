26814b78ec883ac5f71ac7a03a1a6666d8d9fbaa
Aviv Eyal <avivey@gmail.com>
2018-02-20 20:26:26 -0800

    Implement comm as "Raw HID"
    - Add USB raw HID access from PJRC's example code
    - Receive message from teensy in async
    - Send messages to teensy
    - Encode magic as lights

--- windowsnatch/windowsnatch.c
+++ windowsnatch/windowsnatch.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #include "trayicon.h"
+#include "usb_hid.h"
 
 #define LEN_BUFF_SHORT (10)
 #define LEN_BUFF_LONG (50)
@@ -19,29 +20,43 @@ void findMyPutty();
 void HandleWinEvent(HWINEVENTHOOK hook, DWORD event, HWND hwnd,
                     LONG idObject, LONG idChild,
                     DWORD dwEventThread, DWORD dwmsEventTime);
+void WINAPI handleTeensyMessage(DWORD, DWORD, LPOVERLAPPED);
+
+char rawhid_buf[64];
 
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prev, LPSTR cmdline, int show)
 {
   HWND hWnd;
   MSG msg;
-  BOOL bRet;
+  // BOOL bRet;
   int iRetValue = 9;
   HWINEVENTHOOK g_hook;
   DWORD hProc = 0; //putty proc
+
+  int count;
 
   findMyPutty();
   if (targetWindow == NULL) {
     printf("didn't find the target window");
     return 1;
   }
-  showMagic();
 
   GetWindowThreadProcessId(targetWindow, &hProc);
   if (hProc == 0) {
     printf("no process to call\n");
     return 3;
   }
+
+  count = rawhid_open(1, 0x16C0, 0x0486, 0xFFAB, 0x0200);
+  if (count <= 0) {
+    printf("no rawhid device found\n");
+    return 4;
+  }
+  rawhid_async_recv(0, &handleTeensyMessage);
+
+  showMagic();
+
 
   RegisterApplicationClass(hInst);
 
@@ -66,12 +81,37 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prev, LPSTR cmdline, int show)
 
   //  Message loop
   while (TRUE) {
-    bRet = GetMessage(&msg, NULL, 0, 0);
-    if (bRet == 0 || bRet == -1)
-      break;
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
+    DWORD wait_result = MsgWaitForMultipleObjectsEx(0, // no handles
+                        0, // no handles
+                        INFINITE,
+                        QS_ALLINPUT,
+                        MWMO_ALERTABLE | MWMO_INPUTAVAILABLE);
+
+    if (wait_result == WAIT_FAILED) {
+      printf("boom11");
+      continue;
+    }
+
+    while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+      if (msg.message == WM_QUIT) {
+        goto stop_loop;
+      }
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+
+    /*    count = rawhid_recv(rawhid_handle, rawhid_buf, 64, 220);
+        if (count < 0) {
+          printf("teensy gone away\n");
+          rawhid_close(rawhid_handle);
+          break;
+        }
+        if (count > 0) {
+          printf("teensy speaks\n");
+        }*/
   }
+stop_loop:
+
   iRetValue = 0;
 
 cleanup:
@@ -83,8 +123,21 @@ cleanup:
   return iRetValue;
 }
 
+void WINAPI handleTeensyMessage(DWORD dwErr, DWORD cbBytesRead, LPOVERLAPPED lpOverLap) {
+  // printf("got teensy msg\n");
+
+  rawhid_async_recv_complete(0, rawhid_buf, 64);
+
+  // do another one
+  rawhid_async_recv(0, &handleTeensyMessage);
+}
+
 void showMagic()
 {
+  char magic = 10;
+  char sendbuff[64];
+
+
   int len = GetWindowText(targetWindow, buff, LEN_BUFF_LONG);
   // printf("Title len = %d\n[%S]\n", len, buff);
 
@@ -93,21 +146,31 @@ void showMagic()
   switch (magicMarker) {
   case 0:
     printf("Too short");
+    magic = 0;
     break;
 
   case 9:
     printf("tab");
+    magic = 2;
     break;
   case 30:
     printf("Record Seperator");
+    magic = 1;
     break;
   case 31:
     printf("Unit Seperator");
+    magic = 4;
     break;
 
   default:
     printf("Not enough magic: %d", magicMarker);
+    magic = 7;
     break;
+  }
+  if (magic < 10) {
+    sendbuff[0] = magic;
+    printf("\nsending: %d", magic);
+    rawhid_send(0, sendbuff, 64, 100);
   }
   printf("\n");
 }
