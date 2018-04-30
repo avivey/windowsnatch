0b33c6359662f11f68df8856d95f3046a43c88b1
Aviv Eyal <avivey@gmail.com>
2018-04-29 19:45:49 -0700

    All 6 targets

--- teensy/src/main.c
+++ teensy/src/main.c
@@ -3,34 +3,13 @@
 
 #include "keydown.h"
 #include "toolset.h"
+#include "common/compat.h"
 
 #include "common/icd_messages.h"
 
-
-void aviv_debug_number(uint8_t number);
 void animate_error(int count);
 
-int counts[] = {2, 3, 4, 5, 6, 0};
-
-void counter(toolset_t* toolset, void* __) {
-  int n = counts[toolset->id];
-
-  if (is_keydown(toolset->button1)) {
-    n += 1;
-  }
-  if (is_keydown(toolset->button2)) {
-    n -= 1;
-  }
-
-  n = (n + 8  % 8);
-
-  set_led_color(toolset, n);
-
-  counts[toolset->id] = n;
-}
-
-
-void transmit_clicks(toolset_t* toolset) {
+void transmit_clicks(toolset_t* toolset, void* __) {
   int keydown1 = is_keydown(toolset->button1);
   int keydown2 = is_keydown(toolset->button2);
 
@@ -39,7 +18,6 @@ void transmit_clicks(toolset_t* toolset) {
   }
 
   Buffer buffer = clean_and_get_buffer();
-  memset(buffer, 0, RAW_HID_BUFFER_SIZE);
   buffer[0] = ICD_MAGIC_NUMBER;
   buffer[1] = MSG_CODE_BUTTON_PRESS;
   if (keydown1 && keydown2) {
@@ -68,30 +46,11 @@ void transmit_clicks(toolset_t* toolset) {
   }
 }
 
-void reset_if_click(toolset_t* toolset) {
-  if (is_keydown(toolset->button1)) {
-    animate_error(2);
-  }
-  if (is_keydown(toolset->button2)) {
-    _reboot_Teensyduino_();
-  }
-}
-
-void selective_action(toolset_t* toolset, void* p) {
-  switch (toolset->id) {
-  case 5:
-    return reset_if_click(toolset);
-
-  default:
-    return transmit_clicks(toolset);
-  }
-}
-
 int main(void) {
   init_all_led_pins();
   init_buttons();
 
-  animate_error(3);
+  animate_error(2);
 
   while (1) {
     Buffer buffer = get_buffer();
@@ -99,65 +58,9 @@ int main(void) {
       dispatch_incoming_message(buffer);
     }
 
-    if (is_keydown(BUTTON62)) {
-      _reboot_Teensyduino_();
-    }
-
-    if (is_keydown(BUTTON61)) {
-      animate_error(2);
-    }
-
-    iterate_over_all_toolsets(selective_action, NULL);
+    iterate_over_all_toolsets(transmit_clicks, NULL);
     delay(10);
   }
-}
-
-int prev_main(void) {
-  Buffer buffer = clean_and_get_buffer();
-  unsigned int packetCount = 0;
-  while (1) {
-    if (is_keydown(BUTTON62)) {
-      _reboot_Teensyduino_();
-    }
-
-    int bytes = usb_rawhid_recv(buffer, 0); // 0 timeout = do not wait
-    if (bytes > 0) {
-      // the computer sent a message.
-      int n = buffer[0] & 0b111;
-      aviv_debug_number(n);
-    }
-
-    int keydown1 = is_keydown(BUTTON11);
-    int keydown2 = is_keydown(BUTTON12);
-
-    if (keydown1 || keydown2) {
-      // first 2 bytes are a signature
-      buffer[0] = 0xAB;
-      buffer[1] = 0xCD;
-
-      buffer[3] = keydown1;
-      buffer[4] = keydown2;
-
-      // and put a count of packets sent at the end
-      buffer[62] = highByte(packetCount);
-      buffer[63] = lowByte(packetCount);
-
-      // actually send the packet
-      bytes = usb_rawhid_send(buffer, 1000);
-      if (bytes > 0) {
-        packetCount = packetCount + 1;
-      } else {
-        animate_error(5);
-      }
-    }
-
-    delay(10);
-  }
-}
-
-void aviv_debug_number(uint8_t number) {
-  if (number > 7) number = 7;
-  set_led_color(get_toolset(0), number);
 }
 
 void set_led_color_operator(toolset_t* toolset, void* pcolor) {
